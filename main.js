19325e2f5bb0c5860849c12f60a134ee72d1c39f
VyomPaul <86235294+VyomPaul@users.noreply.github.com>
2021-11-11 16:59:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -22,4 +22,45 @@ classifier = ml5.imageClassifier('',modelLoaded);
 function modelLoaded() {
     console.log('Model Loaded!')
 }
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
 
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else{
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+        if(results[0].label == "Amazing Victory")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128522;";
+        }
+        if(results[0].label == "Thumbs up")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128532;";
+        }
+        if(results[0].label == "Very Good")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128848;";
+        }
+        if(results[1].label == "Amazing Victory")
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#128522;";
+        }
+        if(results[1] == "Thumbs Up")
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#128532;";
+        }
+        if(results[1] == "Very Good")
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#128548;";
+        }
+    }
+}
